655779b6edc97734956f0765bba9ef9d488bddec
MatheusBLopes <mathesublopes10@gmail.com>
2022-07-13 10:55:03 -0300

    Update home style and structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,14 +13,13 @@ export default function Home() {
 
         <div className={styles.homeContent}>
           <div className={styles.bioContainer}>
-            <Image src='https://github.com/MatheusBLopes.png' alt='Bio photo' height='1800' width='1800'/>
+            <Image src='https://github.com/MatheusBLopes.png' alt='Bio photo' height='400' width='400'/>
 
             <section>
               <h1>Matheus B. Lopes</h1>
 
               <p>
                 Python developer, graduated in Systems Analysis and Development, passionate about technology. I have public speaking skills, ease of working in a team, willingness to learn and build something new.
-                I&apos;ve been working with systems integration, more specifically marketplaces, where we build the system to integrate orders, products with stock and price and notifications for the seller. The technologies i&apos;ve been working are Python, Microsservices, AWS, Unit Tests, GitHub/GitLab and Grafana.
               </p>
             </section>
           </div>
@@ -29,8 +28,8 @@ export default function Home() {
             <h1>Experience</h1>
 
             <div className={styles.listContainer}>
-              <div className={styles.listItem}><time>From 01/08/2019 to 23/11/2021</time><strong>PUC-PR</strong> <p>Graduated in Systems Analysis and Development</p></div>
-              <div className={styles.listItem}><time>From 23/11/2020 to 23/05/2022</time><strong>Olist</strong> <p>Building APIs and Microsservices with Python and Django</p></div>
+              <div className={styles.listItem}><time>From 01/08/2019 to 23/11/2021</time><p><strong>PUC-PR</strong>Graduated in Systems Analysis and Development</p></div>
+              <div className={styles.listItem}><time>From 23/11/2020 to 23/05/2022</time><p><strong>Olist</strong>I worked with systems integration, more specifically marketplaces, where we build the system to integrate orders, products with stock and price and notifications for the seller. The technologies I worked with were Python, Django, Docker, Microservices, AWS, Unit Tests, GitHub/GitLab and Grafana.</p></div>
             </div>
           </div>
 

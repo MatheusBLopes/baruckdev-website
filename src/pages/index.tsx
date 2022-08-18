422995691b92f14b682559eb4e44a4476588d9a9
MatheusBLopes <mathesublopes10@gmail.com>
2022-08-18 14:13:42 -0300

    Add links section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from './home.module.scss'
 
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <>
@@ -31,6 +34,28 @@ export default function Home() {
               <div className={styles.listItem}><time>From 01/08/2019 to 23/11/2021</time><p><strong>PUC-PR</strong>Graduated in Systems Analysis and Development</p></div>
               <div className={styles.listItem}><time>From 23/11/2020 to 23/05/2022</time><p><strong>Olist</strong>I worked with systems integration, more specifically marketplaces, where we build the system to integrate orders, products with stock and price and notifications for the seller. The technologies I worked with were Python, Django, Docker, Microservices, AWS, Unit Tests, GitHub/GitLab and Grafana.</p></div>
             </div>
+          </div>
+
+          <div className={styles.linksContainer}>
+            <h1>Links</h1>
+
+            <section>
+              <Link
+                href="https://github.com/MatheusBLopes"
+              >
+                <a>
+                  <FaGithub />
+                </a>
+              </Link>
+
+              <Link
+                href="https://www.linkedin.com/in/matheus-bachiste-lopes/"
+              >
+                <a>
+                  <FaLinkedin />
+                </a>
+              </Link>
+            </section>
           </div>
 
 

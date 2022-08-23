8cdd718a89eb04aa054cc66e557f5c5e9f8d4c23
MatheusBLopes <mathesublopes10@gmail.com>
2022-08-22 21:53:26 -0300

    Add loading bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,17 @@ import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
 import '../styles/global.scss'
 
+import NProgress from 'nprogress'
+import '../styles/nprogress.css'
+import { Router } from 'next/router'
+
+//Route Events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
+
+NProgress.configure({ showSpinner: false })
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
